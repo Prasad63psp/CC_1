ed4220afa6ce7cda992588dd2987c018cb50bc6a
Prasad63psp <prasadpowar2003@gmail.com>
2024-10-11 10:46:57 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,6 @@
+class demo{
+  public static void main(string kartu[]){
+  System.out.println("This c program is generated on github\n\n");  
+    System.out.println("This c program is excuted using google app engine\n\n");    
+  }
+  }
