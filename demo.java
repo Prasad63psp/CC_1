bb3d451aa87ef1dc03711cf9ccf5ecb3b0a0138b
Prasad63psp <prasadpowar2003@gmail.com>
2024-10-11 10:48:32 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 class demo{
-  public static void main(string kartu[]){
+  public static void main(String kartu[]){
   System.out.println("This c program is generated on github\n\n");  
-    System.out.println("This c program is excuted using google app engine\n\n");    
+  System.out.println("This c program is excuted using google app engine\n\n");    
   }
   }
