ef91a1ccc2d27fb25ec89fbebe8b7b51f2893220
Prasad63psp <prasadpowar2003@gmail.com>
2024-10-11 10:30:41 +0530

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
-void main()
-print("This c program is generated on github\n\n")
+void main(){
+printf("This c program is generated on github\n\n")
 printf("This c program is excuted using google app engine\n\n") 
+}
